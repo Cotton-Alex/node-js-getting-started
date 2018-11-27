8ecd6cb49412065b4a7ea222707dba646d861a2c
Alex <cot15011@byui.edu>
2018-11-26 22:29:48 -0800

    now with module.export

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ showTimes = () => {
   return result;
 }
 
-updatePrice = function(){
+function updatePrice(){
   var letter_type_val = document.getElementById("letter_type").value;
   var letter_ounce_val = document.getElementById("letter_ounce").value;
   console.log(letter_type_val);
