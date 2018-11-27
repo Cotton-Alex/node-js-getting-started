cff36f3beb1ccd7c117e6d0d282daf8a7bcad7d3
Alex <cot15011@byui.edu>
2018-11-26 22:47:54 -0800

    now with module.export

---
+++ views/pages/functions.js
@@ -0,0 +1,20 @@
+
+function updatePrice(){
+  var letter_type_val = document.getElementById("letter_type").value;
+  var letter_ounce_val = document.getElementById("letter_ounce").value;
+  console.log(letter_type_val);
+  console.log(letter_ounce_val);
+}
+
+
+
+
+
+
+
+
+
+
+module.exports = {
+updatePrice: updatePrice,
+};
