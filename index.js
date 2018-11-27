22869be92c30a8bb9471dc538c0101e469192632
Alex <cot15011@byui.edu>
2018-11-27 00:19:51 -0800

    now with module.export

--- index.js
+++ index.js
@@ -40,3 +40,23 @@ showTimes = () => {
   }
   return result;
 }
+
+function updatePrice(){
+  var letter_type_val = document.getElementById("letter_type").value;
+  var letter_ounce_val = document.getElementById("letter_ounce").value;
+  console.log(letter_type_val);
+  console.log(letter_ounce_val);
+}
+
+
+
+
+
+
+
+
+
+
+module.exports = {
+updatePrice: updatePrice,
+};
